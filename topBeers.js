abb19c1d2a84751ff18dda4e1abd0e6d3573cee5
Uolanb <109627150+Uolanb@users.noreply.github.com>
2025-02-25 13:06:26 +0500

    Add files via upload

---
+++ topBeers.js
@@ -0,0 +1,12 @@
+const topBeers = [
+    "Bud Light (USA)",
+    "Snow Beer (China)",
+    "Budweiser (USA)",
+    "Heineken (Netherlands)",
+    "Corona (Mexico)",
+    "Coors Light (USA)",
+    "Miller Lite (USA)",
+    "Tsingtao (China)",
+    "Asahi Super Dry (Japan)",
+    "Pilsner Urquell (Czech Republic)"
+];
